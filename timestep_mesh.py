106a03c8f21bd8707beb2baa735f7fb2174347ad
Marissa Brown <mbrown36@wisc.edu>
2018-04-03 13:17:41 -0500

    Adding function to generate timestep mesh

---
+++ timestep_mesh.py
@@ -0,0 +1,18 @@
+def generate_timesteps(simulation_length, time_step) 
+"""This function generates a list of timesteps. Step points are spaced evenly between the
+time 0 and the final time simulation_length in steps of time_step 
+
+inputs 
+simulation_time: length of simulation in seconds 
+time_step: timestep in seconds 
+
+outputs 
+time: a list of times 
+""" 
+
+import numpy as np
+ 
+time = np.arange(0, simulation_length, time_step)
+ 
+return time
+
