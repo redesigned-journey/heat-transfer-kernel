0c052010aeb093f6f9816d3f699ae0f0c9d7df31
Zach Fiscus <zfiscus@wisc.edu>
2018-04-13 16:34:06 -0500

    Updated mesh generator to reflect different formatting choice for mesh.

--- generate_mesh.py
+++ generate_mesh.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def generate_mesh(layer_outer_radii, nodes_per_layer):
+def generate_mesh(layer_outer_radii, nodes_per_layer, materials):
     """ This function generates an array of grid points in the radial direction,
     or mesh.
     Grid points are spaced evenly between the previous outer radii
@@ -14,19 +14,27 @@ def generate_mesh(layer_outer_radii, nodes_per_layer):
     The first layer is assumed to start at r=0
     - nodes_per_layer : list with the number of grid points
       corresponding to each layer
+    - materials : a list of the materials in the order in which they appear
 
     outputs
     -------
-    - mesh : a list of grid points
+    - mesh : a list of tuples, each tuple is composed of the material and nodes associated with that material
     """
-
-    mesh = np.zeros(sum(nodes_per_layer))
-    nodes_sum = 0
-    for i in range(1, len(layer_outer_radii)):
+    mesh = list()
+    for i in range(1, len(materials)+1):
+        mesh_points = np.linspace(layer_outer_radii[i-1], layer_outer_radii[i], nodes_per_layer[i-1])
+        mesh.append((materials[i-1], mesh_points)) 
+        
+        
+        
+        
+        
+        
+    """for i in range(1, len(layer_outer_radii)):
         nodes_sum_new = nodes_sum+nodes_per_layer[i-1]
         single_material_nodes = np.linspace(layer_outer_radii[i-1],
                                             layer_outer_radii[i],
                                             nodes_per_layer[i-1])
         mesh[nodes_sum:nodes_sum_new] = single_material_nodes
-        nodes_sum = nodes_sum_new
+        nodes_sum = nodes_sum_new"""
     return mesh
