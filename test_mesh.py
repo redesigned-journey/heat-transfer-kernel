50a9013c9ad09a8184de124020d5bfac7b1fbbb1
Zach Fiscus <zfiscus@wisc.edu>
2018-04-16 11:11:16 -0500

    Updated tests. Still don't work but they reflect new form of mesh generator

--- test_mesh.py
+++ test_mesh.py
@@ -4,14 +4,17 @@ import pytest
 from generate_mesh import generate_mesh
 
 def test_generic():
-	expected = np.array([0, 0.5, 1])
-	layer_outer_radii = [0, 1]
-	nodes_per_layer = [3]
+    materials = ['Vibranium']
+    expected = [('Vibranium',np.array([0, 0.5, 1]))]
+    layer_outer_radii = [0, 1]
+    nodes_per_layer = [3]
+    assert np.all([ a==b for a,b in zip(expected, generate_mesh(layer_outer_radii,nodes_per_layer, materials))])
 	
-	assert np.all(expected == generate_mesh(layer_outer_radii,nodes_per_layer))
+	#assert np.all(expected == generate_mesh(layer_outer_radii,nodes_per_layer, materials))
 	
 def test_several_materials():
    layer_outer_radii = [0, 1, 2, 3]
    nodes_per_layer= [3, 2, 5]
-   expected = np.array([0, 0.5, 1, 1, 2, 2, 2.25, 2.5, 2.75, 3])
-   assert np.all(expected == generate_mesh(layer_outer_radii,nodes_per_layer))
+   materials = ['Vibranium', 'Unobtanium', 'Europium']
+   expected = (('Vibranium',np.array([0, 0.5, 1])),('Unobtanium', np.array([1, 2])), ('Europium', np.array([2, 2.25, 2.5, 2.75, 3])))
+   assert np.all(expected == generate_mesh(layer_outer_radii,nodes_per_layer, materials))
