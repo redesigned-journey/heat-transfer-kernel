e31cd80ce4086b122dcd9c393193ffc3bf88b63a
Zach Wolf <zwolf2@wisc.edu>
2018-04-26 11:39:43 -0500

    Updated materials to more descriptive name

--- src/materials_dict_master.py
+++ src/materials_dict_master.py
@@ -1,8 +1,8 @@
 materials_dict_master = { 'UO2': {'rho':10.5, 'k': 2.5, 'c': 332},
-                        { 'PCB': {'rho':1.00, 'k': 0.5, 'c': 1.5},
-                        { 'IPC': {'rho':1.90, 'k': 4.0, 'c': 1.5},
-                        { 'SC' : {'rho':3.20, 'k': 13.9, 'c': 0.5},
-                        { 'PC' : {'rho':1.90, 'k': 4.0, 'c': 1.5},
+                        { 'Porous Carbon Buffer': {'rho':1.00, 'k': 0.5, 'c': 1.5},
+                        { 'Innner Pyrolytic Carbon': {'rho':1.90, 'k': 4.0, 'c': 1.5},
+                        { 'Silicon Carbide' : {'rho':3.20, 'k': 13.9, 'c': 0.5},
+                        { 'Pyrolytic Carbon' : {'rho':1.90, 'k': 4.0, 'c': 1.5},
 
 
 
