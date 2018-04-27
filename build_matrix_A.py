Fixed a sign error that was only apparent when attempting to modify to work with transients.

build_matrix_A.py
@@ -37,7 +37,6 @@ def build_matrix_A(material_property_library, mesh, time):
         total_nodes += len(mesh[i][1])-1
         
     A = np.zeros((total_nodes, total_nodes))
-    A_test = np.zeros((3,total_nodes))
     volume = np.zeros(total_nodes)
     
     "Begin by setting boundary nodes"
@@ -45,23 +44,23 @@ def build_matrix_A(material_property_library, mesh, time):
     volume[0] = 4/3*np.pi*(mesh[0][1][0]+DR[0]/2)**3
     volume[1] = 4/3*np.pi*(mesh[0][1][1]+DR[0]/2)**3-(mesh[0][1][1]-DR[0]/2)**3
     
-    A[0, 0] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[0])*(DR[0]/2)**2
-    A[0, 1] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[0])*(DR[0]/2)**2
+    A[0, 0] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[0])*(DR[0]/2)**2
+    A[0, 1] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[0])*(DR[0]/2)**2
 	
 
-    A[1, 1] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[1]*(1/(1/mesh[0][1][1]-1/mesh[0][1][2]) +
+    A[1, 1] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[1]*(1/(1/mesh[0][1][1]-1/mesh[0][1][2]) +
                                     1/DR[0]*(mesh[0][1][0]+DR[0]/2)**2)
-    A[1, 0] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[1])*(mesh[0][1][0]+DR[0]/2)**2
-    A[1, 2] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[1]*(1/(1/mesh[0][1][1]-1/mesh[0][1][2]))
+    A[1, 0] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/(DR[0]*volume[1])*(mesh[0][1][0]+DR[0]/2)**2
+    A[1, 2] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[1]*(1/(1/mesh[0][1][1]-1/mesh[0][1][2]))
 	
 
 	
     for i in range(2, len(mesh[0][1])-1): #Length set to fueled node length
         volume[i] = 4/3*np.pi*((mesh[0][1][i]+DR[0]/2)**3-(mesh[0][1][i]-DR[0]/2)**3)
-        A[i, i] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i]-1/mesh[0][1][i+1]) +
+        A[i, i] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i]-1/mesh[0][1][i+1]) +
                                         1/(1/mesh[0][1][i-1]-1/mesh[0][1][i]))
-        A[i, i-1] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i-1]-1/mesh[0][1][i]))
-        A[i, i+1] = k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i]-1/mesh[0][1][i+1]))
+        A[i, i-1] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i-1]-1/mesh[0][1][i]))
+        A[i, i+1] = -k[0]*Dt*4*np.pi/(rho[0]*c[0])/volume[i]*(1/(1/mesh[0][1][i]-1/mesh[0][1][i+1]))
 		
     #Now need to handle the boundary nodes
     #i is currently the value of the node at the internal boundary
@@ -69,23 +68,23 @@ def build_matrix_A(material_property_library, mesh, time):
         prev_mesh_length = len(mesh[mat-1][1])
         volume[i+1] = 4/3*np.pi*((mesh[mat-1][1][prev_mesh_length-1]+DR[mat]/2)**3-(
                                                  mesh[mat-1][1][prev_mesh_length-1]-DR[mat-1]/2)**3)
-        A[i+1,i+2] = k[mat]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat][1][0]-1/mesh[mat][1][1]))
-        A[i+1,i+1] = (-k[mat-1]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat-1][1][prev_mesh_length-2]
-                                                    -1/mesh[mat-1][1][prev_mesh_length-1]))-(
+        A[i+1,i+2] = -k[mat]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat][1][0]-1/mesh[mat][1][1]))
+        A[i+1,i+1] = (k[mat-1]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat-1][1][prev_mesh_length-2]
+                                                    -1/mesh[mat-1][1][prev_mesh_length-1]))+(
                                                     k[mat]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat][1][0]
                                                     -1/mesh[mat][1][1]))))
-        A[i+1, i] = k[mat-1]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat-1][1][prev_mesh_length-2]-
+        A[i+1, i] = -k[mat-1]*Dt*4*np.pi/(rho[mat-1]*c[mat-1])/volume[i+1]*(1/(1/mesh[mat-1][1][prev_mesh_length-2]-
                                                                         1/mesh[mat-1][1][prev_mesh_length-1]))
 	
         for j in range(1, len(mesh[mat][1])-1): 
             volume[j+i+1] = 4/3*np.pi*((mesh[mat][1][j]+DR[mat]/2)**3-(mesh[mat][1][j]-DR[mat]/2)**3)
-            A[j+i+1, j+i+1] = -k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j]-1/mesh[mat][1][j+1]) +
+            A[j+i+1, j+i+1] = k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j]-1/mesh[mat][1][j+1]) +
                                         1/(1/mesh[mat][1][j-1]-1/mesh[mat][1][j]))
-            A[j+i+1, j-1+i+1] = k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j-1]-1/mesh[mat][1][j]))
-            A[j+i+1, j+1+i+1] = k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j]-1/mesh[mat][1][j+1]))
+            A[j+i+1, j-1+i+1] = -k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j-1]-1/mesh[mat][1][j]))
+            A[j+i+1, j+1+i+1] = -k[mat]*Dt*4*np.pi/(rho[mat]*c[mat])/volume[j+i+1]*(1/(1/mesh[mat][1][j]-1/mesh[mat][1][j+1]))
         i+=j+1
 
-    A[total_nodes-1,total_nodes-1] = 1
+    A[total_nodes-1, total_nodes-1] = 1
 
 
     return A

build_matrix_b.py
@@ -32,7 +32,7 @@ def build_matrix_b(boundary_conditions, mesh, material_property_library, g_dot, 
         total_nodes += len(mesh[i][1])-1
     b = np.zeros(total_nodes)
     for i in range(len(mesh[0][1])):
-        b[i] = -g_dot*Dt/(rho[0]*c[0])
+        b[i] = g_dot*Dt/(rho[0]*c[0])
     b[total_nodes-1] = boundary_conditions
 
     return b