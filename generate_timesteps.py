c69669ee085e5b4e3627d7d637251d5c4430aa11
Marissa Brown <mbrown36@wisc.edu>
2018-04-30 11:55:35 -0500

    Adding changes to timestep function

--- generate_timesteps.py
+++ generate_timesteps.py
@@ -1,20 +1,19 @@
 import numpy as np
+import sys
 
 
-def generate_timesteps(simulation_length, time_step):
+def generate_timesteps(simulation_length, num_steps):
     """This function generates a list of timesteps. Step points are spaced evenly
     between the time 0 and the final time simulation_length in steps of
     time_step
 
     inputs
     -simulation_time: length of simulation in seconds
-    -time_step: timestep in seconds
+    -num_steps: the number of timesteps
 
     outputs
     -time: a list of times
     """
 
-    time = np.arange(0, simulation_length, time_step)
-
+    time = np.linspace(0, simulation_length, num=num_steps)
     return time
-
