aa4b3384ac5caa4ab22bec2d7e3877f2bb36a3f6
Zach Fiscus <zfiscus@wisc.edu>
2018-04-30 10:41:31 -0500

    Updated docstring.

--- transient_model.py
+++ transient_model.py
@@ -2,26 +2,32 @@ import numpy as np
 from build_matrix_A import build_matrix_A
 from build_matrix_b import build_matrix_b
 from matrix_solver import solve_matrix
-from build_A_analytical import build_A_analytical
 from generate_mesh import generate_mesh
 
 def transient_model(time, material_property_library, mesh, g_dot_list, boundary_conditions_list):
-    "Will also have to pass in however bcs or heat generation rate change with time"
-    "Begin by setting up the initial conditions"
     """This function will calculate the transient response of the fuel particle to changes in either heat
     generation rate or to boundary wall temperature. The A matrix has 1 added to its diagonal entries while
-    the b matrix has the previous temperature set added to its entries.
+    the b matrix has the previous temperature set added to its entries. These changes need to be made now that 
+    dT/dt is no longer zero:
+    
+    T_time+1 = T_time + dT/dt *Delta_t
+    
+    Where previously dT/dt was equal to zero, the matrices simplified dramatically. Now this is not the case,
+    so the b matrix must be appended with the known constant T_time and the A matrix must be appended with 1 
+    along the diagonal to account for the presence of T_time+1.
        
     inputs
     ------
     - time: An array of the time steps taken by the solver. Passed through to the matrix generators.
     - mesh: the array of points for the physical location of nodes and the materials assigned to them
     - material_property_library: a dictionary or nested dictionaries of the materials used in the modeled region
-    - g_dot_vec: a list with the heat generation rates before the transient and during the transient
+    - g_dot_list: a list with the heat generation rates before the transient and during the transient. g_dot_list 
+    is of length 2, with the 0th entry being prior to the transient and fist entry after time zero.
     - boundary_condtions_list: a list with the boundary temperature before the transient and during the transient
-    outputs
+    outputs. This list is of length 2, with the 0th entry being prior to the transient and fist entry after time zero.
     -T: numpy array containing the temperatures for each node and time
-    """    
+    """  
+    
     total_nodes = len(mesh[0][1])
     for i in range(1,len(mesh)):
         total_nodes += len(mesh[i][1])-1
@@ -45,3 +51,4 @@ def transient_model(time, material_property_library, mesh, g_dot_list, boundary_
         T[:,i] = solve_matrix(A, b)
     
     return T
+    
