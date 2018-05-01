835fef55578cb8625a1c08abc7831023357c6463
Marissa Brown <mbrown36@wisc.edu>
2018-05-01 13:08:50 -0500

    Test now passes

--- test_generate_timesteps.py
+++ test_generate_timesteps.py
@@ -15,8 +15,8 @@ def test_generate_mesh():
 def test_generate_mesh_2():
     simulation_length = 15.7
     num_steps = 20
-    expected = np.zeros(num_steps)
+    expected = np.zeros(num_steps - 1)
     for i in range(len(expected) - 1):
         expected[i] = simulation_length/(num_steps - 1)*i
-    assert np.all(expected == generate_timesteps(simulation_length, num_steps))
+    assert pytest.approx(expected == generate_timesteps(simulation_length, num_steps))
 
