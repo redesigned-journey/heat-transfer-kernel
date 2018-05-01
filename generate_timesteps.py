6ebd29b1b47f91f64b890574ba9d865b210780d1
Marissa Brown <mbrown36@wisc.edu>
2018-05-01 11:54:40 -0500

    Changes to docstring

--- generate_timesteps.py
+++ generate_timesteps.py
@@ -4,8 +4,8 @@ import sys
 
 def generate_timesteps(simulation_length, num_steps):
     """This function generates a list of timesteps. Step points are spaced evenly
-    between the time 0 and the final time simulation_length in steps of
-    time_step
+    between the time 0 and the final time simulation_length based on the number of
+    timesteps num_steps.
 
     inputs
     -simulation_time: length of simulation in seconds
