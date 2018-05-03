5db1082562ab0620512ec23b638a848600346b3f
robert <rarmstrong3@wisc.edu>
2018-05-02 22:20:03 -0500

    Updates to add the transient model.

--- src/main_prog.py
+++ src/main_prog.py
@@ -14,10 +14,15 @@ import matrix_solver
 
 import plotting_func
 
+import transient_model
+
+import generate_timesteps
+
 
 # Allow for user input for material information and layers per node
 
 parser = argparse.ArgumentParser()
+
 
 parser.add_argument("-f", "--fuel", type=str, default='UO2', required=False,
 	               choices=material_properties.fuel_props_master.keys(),
@@ -38,14 +43,20 @@ parser.add_argument("-ct", "--clad_radii", nargs='+', default = [280E-06 , 315E-
 parser.add_argument("-ln", "--layers_per_node", nargs='+', default=[5,4,3],
 	                help='Determines the layers_per_node')
 
-parser.add_argument("-tr","--transient", nargs='+', default=[],required=False,
-	                help="Runs transient model. Enter simulation time and then timestep seperated by a space")
+parser.add_argument("-tr","--transient", nargs='+', type=float, default=[],required=False,
+	                help="Runs transient model. Enter simulation time and then number of timesteps seperated by a space")
 
 parser.add_argument("-bt", "--boundary_temp", type=float, default=1800.00,
 	                help="Determines the temperature boundary condition at the outside of the particle")
 
 parser.add_argument("-gd", "--g_dot", type=float, default=1.1E10,
 	                help="Determines the generation in the fuel")
+
+parser.add_argument("-tg","--transient_gen", nargs='+', type=float, default=[1.1E10,1800],required=False,
+	                help="Runs transient model. Enter simulation time and then \
+	                number of timesteps seperated by a space")
+
+
 
 
 
@@ -70,7 +81,7 @@ material_props = material_properties.material_property_library(material_info)
 
 mesh = generate_mesh.generate_mesh(material_props,layers_per_node)
 
-print(mesh)
+print(args.transient)
 
 boundary_condition = boundary_conditions.boundary_conditions(args.clad_radii[-1], args.boundary_temp)
 
@@ -86,23 +97,10 @@ if len(args.transient) == 0:
 
 #    print(temperature_array)
 
-plotting_func.temperature_plot(temperature_array, mesh)
+    plotting_func.temperature_plot(temperature_array, mesh)
 
-#elif len(args.t)
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
+elif len(args.transient) == 2:
+	time = generate_timesteps.generate_timesteps(args.transient[0],args.transient[1])
+	g_dot_list = [args.g_dot, args.transient_gen[0]]
+	temp_boundary_list = [args.boundary_temp,args.transient_gen[1]]
+	temperature_array = transient_model.transient_model(time, material_info, mesh, g_dot_list, temp_boundary_list)
